50f04d94f5a4d8493c3338a0c9441864bf816ac6
EmilyHuffel <156906721+EmilyHuffel@users.noreply.github.com>
2024-01-17 12:22:35 +0100

    Update main.py

--- main.py
+++ main.py
@@ -17,14 +17,14 @@ def load_data():
         f"""
             select 
                 res.colophon_data_city as city, 
-                date_trunc('day', rev.datetime) as review_day,
+                DATE(rev.datetime) as review_day,
                 count(*) as n_reviews
             from 
                 public.reviews rev
                 join restaurants res
                 using (restaurant_id)
             where res.colophon_data_city in ('Amsterdam', 'Rotterdam', 'Groningen') and rev.datetime >= '2022-01-01' and rev.datetime < '2023-01-01'
-            group by res.colophon_data_city, review_day 
+            group by review_day, res.colophon_data_city 
             """,  # noqa: F541
         con=engine,
     )
@@ -39,28 +39,29 @@ data_load_state = st.text("Loading data...")
 data = load_data()
 
 # Filter Data on Date
-data["review_day"] = data["review_day"].dt.date
+# data["review_day"] = data["review_day"].dt.date
 date_min, date_max = st.slider(
     label="Included date range",
     min_value=datetime.date(2022, 1, 1),
-    max_value=datetime.date(2023, 1, 1),
-    value=(datetime.date(2022, 1, 1), datetime.date(2023, 1, 1)),
-    step=datetime.timedelta(days=1),
+    max_value=datetime.date(2022, 12, 31),
+    value=(datetime.date(2022, 1, 1), datetime.date(2022, 12, 31)),
 )
 # date_min = st.date_input(label="earliest included date", value=datetime.date(2022, 1, 1))
 # date_max = st.date_input(label="last included date", value=datetime.date(2022, 12, 31)) + datetime.timedelta(
-# days=1
+#     days=1
 # )
 
-filtered_data = data[(data["review_day"] >= date_min) & (data["review_day"] < date_max)]
-
+filtered_data = data.loc[((data["review_day"] >= date_min) & (data["review_day"] <= date_max))].sort_values(
+    by="review_day"
+)
+print(filtered_data)
 
 # Notify the reader that the data was successfully loaded.
 data_load_state.text("Loading data...done!")
 
 
 # Create summed data and plot it
-data_summed = filtered_data[["city", "n_reviews"]].groupby("city").sum().reset_index()
+data_summed = filtered_data[["city", "n_reviews"]].groupby("city", as_index=False).sum()
 st.bar_chart(data_summed, x="city", y="n_reviews")
 
 # Create a line chart of reviews over time
